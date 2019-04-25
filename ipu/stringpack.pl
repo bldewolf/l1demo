36b4593cc41a5c751b5a93e78c15a35e0b4f1f9f
Brian De Wolf <git@bldewolf.com>
2019-04-24 18:09:58 -0700

    fix string packer

--- ipu/stringpack.pl
+++ ipu/stringpack.pl
@@ -5,8 +5,8 @@ use strict;
 
 my $x = 0;
 while(defined(my $c = getc())) {
-	$c = unpack("C", $c)
-	printf "0x%x,", $c;
+	$c = unpack("C", $c);
+	printf "0x%02x, ", $c;
 	if(++$x > 8) {
 		$x = 0;
 		printf "\n";
